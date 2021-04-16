3e876622260e05bab0933c62f498c6339bb84770
Diego Buenrostro <dbuenrostro64@gmail.com>
2021-04-16 15:50:59 -0700

    pygame window setup

--- snake.py
+++ snake.py
@@ -1,3 +1,17 @@
+import pygame, sys
+from pygame.locals import *
+
+
+def main():
+    pygame.init()
+    DISPLAYSURF = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption('Hello World!')
+    while True: # main game loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
 
 if __name__ == '__main__':
     main()
