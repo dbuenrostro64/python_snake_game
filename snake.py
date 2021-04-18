158c7267b42401e75d9a9124ac1cfd977551cfcd
Diego Buenrostro <dbuenrostro64@gmail.com>
2021-04-18 09:41:07 -0700

    square keeps moving with one key press

--- snake.py
+++ snake.py
@@ -22,6 +22,7 @@ def main():
     square_pos_y = window_height/2
     square_side = 30
     square_velocity = 5
+    snake_direction = "none"
 
     while True: # main game loop
         pygame.time.delay(100)
@@ -31,15 +32,24 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        key_pressed = pygame.key.get_pressed()
-        if key_pressed[pygame.K_UP]:
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    snake_direction = "up"
+                elif event.key == pygame.K_DOWN:
+                    snake_direction = "down"
+                elif event.key == pygame.K_LEFT:
+                    snake_direction = "left"
+                elif event.key == pygame.K_RIGHT:
+                    snake_direction = "right"            
+
+        if snake_direction == "up":
             square_pos_y -= square_velocity
-        elif key_pressed[pygame.K_DOWN]:
+        elif snake_direction == "down":
             square_pos_y += square_velocity
-        elif key_pressed[pygame.K_LEFT]:
+        elif snake_direction == "left":
             square_pos_x -= square_velocity
-        elif key_pressed[pygame.K_RIGHT]:
-            square_pos_x += square_velocity            
+        elif snake_direction == "right":
+            square_pos_x += square_velocity    
 
         background.fill(orange)   
         pygame.draw.rect(background, green, (square_pos_x, square_pos_y, square_side, square_side))             
