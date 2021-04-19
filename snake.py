5d78b4a1760b742363269defcac392ef85d8f474
Diego Buenrostro <dbuenrostro64@gmail.com>
2021-04-18 21:28:05 -0700

    food moves to random location when eaten

--- snake.py
+++ snake.py
@@ -1,40 +1,49 @@
 import pygame, sys, random
 from pygame.locals import *
 
+
 class Snake:
 
     def __init__(self, win_size):
 
-        self.snake_pos_x = win_size[0]/2
-        self.snake_pos_y = win_size[1]/2
-        self.snake_block_size = 30
-        self.snake_velocity = 10
-        self.snake_direction = "none"
+        self.pos_x = win_size[0]/2
+        self.pos_y = win_size[1]/2
+        self.block_size = 20
+        self.velocity = 20
+        self.direction = "none"
 
     def add_block(self):
         pass
+
 
 class Food:
 
     def __init__(self, win_size):
         self.window_width = win_size[0]
         self.window_height = win_size[1]
-        self.food_pos_x = 100
-        self.food_pos_y = 100
-        self.food_size = 15
+        self.pos_x = 100
+        self.pos_y = 100
+        self.size = 20
 
-    def randomize_food(self):
-        self.food_pos_x = random.randrange(1, (window_width//10)) * 10
-        self.food_pos_y = random.randrange(1, (window_height//10)) * 10
+    def randomize(self):
+        self.pos_x = random.randrange(0, self.window_width, 20) 
+        self.pos_y = random.randrange(0, self.window_height, 20)
 
     def spawn_food(self):
         pass
 
+
+def collision_check(snake, food):
+    if snake.pos_x == food.pos_x and snake.pos_y == food.pos_y: 
+        return True
+    else:
+        return False
+
 def main():
     # window size
-    window_width = 800
-    window_height = 600
-    window_size = (window_width, window_height)
+    WINDOW_WIDTH = 800
+    WINDOW_HEIGHT = 600
+    WINDOW_SIZE = (window_width, window_height)
 
     # colors
     black = pygame.Color(0, 0, 0)
@@ -46,16 +55,11 @@ def main():
     # game init
     pygame.init()
     pygame.display.set_caption('Snake Game')
-    background = pygame.display.set_mode((window_width, window_height))
+    background = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     background.fill(orange)
 
-    square_pos_x = window_width/2
-    square_pos_y = window_height/2
-    square_side = 30
-    square_velocity = 10
-    snake_direction = "none"
-
-    food_1 = Food(window_size)
+    snake_1 = Snake(WINDOW_SIZE)
+    food_1 = Food(WINDOW_SIZE)
 
     while True: # main game loop
         pygame.time.delay(100)
@@ -67,26 +71,31 @@ def main():
 
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    snake_direction = "up"
+                    snake_1.direction = "up"
                 elif event.key == pygame.K_DOWN:
-                    snake_direction = "down"
+                    snake_1.direction = "down"
                 elif event.key == pygame.K_LEFT:
-                    snake_direction = "left"
+                    snake_1.direction = "left"
                 elif event.key == pygame.K_RIGHT:
-                    snake_direction = "right"            
+                    snake_1.direction = "right"            
 
-        if snake_direction == "up":
-            square_pos_y -= square_velocity
-        elif snake_direction == "down":
-            square_pos_y += square_velocity
-        elif snake_direction == "left":
-            square_pos_x -= square_velocity
-        elif snake_direction == "right":
-            square_pos_x += square_velocity    
+        if snake_1.direction == "up":
+            snake_1.pos_y -= snake_1.velocity
+        elif snake_1.direction == "down":
+            snake_1.pos_y += snake_1.velocity
+        elif snake_1.direction == "left":
+            snake_1.pos_x -= snake_1.velocity
+        elif snake_1.direction == "right":
+            snake_1.pos_x += snake_1.velocity   
+
+        # situation check
+        collision = collision_check(snake_1, food_1)
+        if collision:
+            food_1.randomize()
 
         background.fill(orange)   
-        pygame.draw.rect(background, green, (square_pos_x, square_pos_y, square_side, square_side)) 
-        pygame.draw.rect(background, red, pygame.Rect(food_1.food_pos_x, food_1.food_pos_y, food_1.food_size, food_1.food_size))            
+        pygame.draw.rect(background, green, pygame.Rect(snake_1.pos_x, snake_1.pos_y, snake_1.block_size, snake_1.block_size)) 
+        pygame.draw.rect(background, red, pygame.Rect(food_1.pos_x, food_1.pos_y, food_1.size, food_1.size))            
         pygame.display.update()
 
 if __name__ == '__main__':
