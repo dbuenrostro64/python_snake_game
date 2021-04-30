c81dfee0a0610e6bd69d90570b3bd49795409f4f
Diego Buenrostro <dbuenrostro64@gmail.com>
2021-04-30 14:22:31 -0700

    saved high scores with score and custom name

--- snake.py
+++ snake.py
@@ -58,8 +58,8 @@ def start_screen(surface, font):
                 if event.key == pygame.K_SPACE:
                     start = False
         surface.fill(colors.purple)
-        font.render_to(surface, (150, 100), "SNAKE GAME!!!", colors.black, None, size=75)
-        font.render_to(surface, (125, 400), "Press space to start", colors.black, None, size=50)
+        font.render_to(surface, (150, 100), "SNAKE GAME!!!", colors.green, None, size=75)
+        font.render_to(surface, (125, 400), "Press space to start", colors.green, None, size=50)
         pygame.display.update()
 
 def rules_screen(surface, font):
@@ -68,20 +68,42 @@ def rules_screen(surface, font):
 
 def game_over(surface, font, score, high_scores):
     """Draws game over when triggered """
-    high_scores.append(score)
-    leaderboard.save_scores(high_scores)
+    
+    letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+                'O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    letter_pos = 0
+    name = ""
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_RETURN:
+
+                    high_scores.append((name, score))
+                    leaderboard.save_scores(high_scores)
                     leaderboard_screen(surface, font, score, high_scores)
+                elif event.key == pygame.K_RIGHT:
+                    letter_pos += 1
+                    if letter_pos == 26:
+                        letter_pos = 0
+                elif event.key == pygame.K_LEFT:
+                    letter_pos -= 1
+                    if letter_pos == -26:
+                        letter_pos = 0
+                elif event.key == pygame.K_SPACE:
+                    name = name + letters[letter_pos]
         surface.fill(colors.black)
         str_score=str(score)
-        font.render_to(surface, (125, 100), "GAME OVER", colors.red, None, size=100)
-        font.render_to(surface, (175, 425), "Score: "+str_score, colors.red, None, size=100)
+        font.render_to(surface, (175, 50), "GAME OVER", colors.red, None, size=80)
+        font.render_to(surface, (215, 175), "Score: "+str_score, colors.red, None, size=80)
+        font.render_to(surface, (100, 280), "Left and right arrow to cycle letters", colors.white, None, size=30)
+        font.render_to(surface, (275, 325), "Space to enter", colors.white, None, size=30)
+        font.render_to(surface, (275, 375), "Enter Name: " + letters[letter_pos], colors.blue, None, size=30)
+        font.render_to(surface, (315, 475), name, colors.blue, None, size=30)
+        font.render_to(surface, (250, 525), "enter to confirm", colors.white, None, size=30)
+
         pygame.display.update()
 
 def leaderboard_screen(surface, font, score, high_scores):
@@ -91,11 +113,11 @@ def leaderboard_screen(surface, font, score, high_scores):
     font.render_to(surface, (150, 125), "NAME", colors.purple, None, size=25)
     font.render_to(surface, (550, 125), "SCORE", colors.purple, None, size=25)
 
-    high_scores.sort(reverse=True)
+    sorted_high_scores = sorted(high_scores, key = lambda x: x[1], reverse=True)
     height = 175
-    for scr in high_scores:
-        scr_str=str(scr)
-        font.render_to(surface, (150, height), "Player" , colors.blue, None, size=25)
+    for scr in sorted_high_scores:
+        scr_str=str(scr[1])
+        font.render_to(surface, (150, height), scr[0] , colors.blue, None, size=25)
         font.render_to(surface, (550, height), scr_str, colors.blue, None, size=25)
         height += 30
     pygame.display.update()
@@ -120,7 +142,6 @@ def main():
     pygame.init()
     pygame.display.set_caption('Snake Game')
     background = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    background.fill(colors.orange)
     font_obj = font_init()
     play_again = True
 
@@ -174,7 +195,7 @@ def main():
 
         seconds = (pygame.time.get_ticks()-start_tick)/1000
         seconds = round(seconds)
-        background.fill(colors.orange)
+        background.fill(colors.blue)
         display_score(background, font_obj, colors.black, score)
         display_time(background, font_obj, colors.white, seconds)
         snake_1.draw()
